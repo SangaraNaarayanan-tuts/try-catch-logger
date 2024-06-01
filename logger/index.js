7ff1c9acd93cc34548068e5054e96ecd9990242f
Sangara <sangara@kbxdigital.com>
2024-06-01 16:40:40 +0530

    based version of the package with the logger and the main function

---
+++ logger/index.js
@@ -0,0 +1,17 @@
+const logger = {
+  currentTime: new Date().toLocaleString(),
+  info: function (details) {
+    console.info(`[INFO] [${this.currentTime}] ${details}`);
+  },
+  success: function (details) {
+    console.log(`[SUCCESS] [${this.currentTime}] ${details}`);
+  },
+  error: function (details, error) {
+    console.error(`[ERROR] [${this.currentTime}] ${details}`);
+    if (error) {
+      console.log(JSON.stringify(error, null, 5));
+    }
+  }
+};
+
+module.exports = logger;
